e57ab9477552566729276e2e054cc41ee8021956
Manuel Santamaría González <lolosg95@gmail.com>
2022-06-25 04:39:20 +0200

    Maquetación formulario

--- blogViajes/src/app/components/formulario/formulario.component.ts
+++ blogViajes/src/app/components/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+  nuevoPost: FormGroup;
+
+  constructor() {
+    this.nuevoPost = new FormGroup({
+      title: new FormControl('', [
+        Validators.required
+      ]),
+      description: new FormControl('', [
+        Validators.required,
+        Validators.minLength(120),
+      ]),
+      author: new FormControl('', [
+        Validators.required
+      ]),
+      image: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/)
+      ]),
+      category: new FormControl('', [
+        Validators.required
+      ]),
+
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  checkValidator() {
+
+  }
+
+  getDataForm() {
+
   }
 
 }
